0af1b5fb696150d37fe1dddb026a088082b5674f
kdieppa <dieppak@mit.edu>
2018-03-02 13:17:55 -0500

    working hw submission

--- exercise.js
+++ exercise.js
@@ -14,20 +14,10 @@ exercise.roman = function(time){
     min_col2  = Number(time.toTimeString()[4]), 
     sec_col1  = Number(time.toTimeString()[6]), 
     sec_col2  = Number(time.toTimeString()[7])
-    //  For example, for 05:13:47 PM
-    //    { 
-    //       hour_col1 : 1, 
-    //       hour_col2 : 7,
-    //       min_col1  : 1, 
-    //       min_col2  : 3, 
-    //       sec_col1  : 4, 
-    //       sec_col2  : 7
-    //     };
-    // 
-    //-------------------------------------
-
-
+    
+    return [hour_col1,hour_col2,min_col1,min_col2,sec_col1,sec_col2];
 };
+
 
 exercise.binary = function(time, col){
 
@@ -37,25 +27,30 @@ exercise.binary = function(time, col){
     //    the binary clock values 
     //    for the given column
     
+var position8 = false;
+var position4 = false;
+var position2 = false;
+var position1 = false;
+
 exercise.roman(time);
 
 if(col>=8){
-    document.getElementById((col+"_pos8")).background = 'red'
+    position8 = true
     col -= 8;
 }
 if(col>=4){
-    document.getElementById((col+"_pos4")).background = 'red'
+    position4 = true
     col -= 4;
 }
 if(col>=2){
-    document.getElementById((col+"_pos2")).background = 'red'
+    position2 = true
     col -= 2;
 }
 if(col>=1){
-    document.getElementById((col+"_pos1")).background = 'red'
+    position1 = true
     col -= 1;
 }
 
-
+return [position8, position4, position2, position1];
 
 }
